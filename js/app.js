f0e07ec6cc9c2e245354e60214416553027b92ef
stickfr <20114698+stickfr@users.noreply.github.com>
2023-09-16 21:27:02 -0400

    added slider bars, better css, and a temporary output!

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
 let search, button, logo;
+let options = [];
+let optioning = false;
+let generation = null;
+
+let spantext = "";
+
+let option_labels = [
+  'Overall',
+  'Challenge',
+  'Workload',
+  'Year'
+]
 
 let circles = [];
 const MAX_CIRCLES = 20;
@@ -26,59 +38,112 @@ function setup() {
 
   search = createInput();
   search.attribute('placeholder', 'What\'s your ideal college experience?');
-  search.style(`
-    border-radius: 25px;
-    border: 0;
-    font-size: 24px;
-    font-family: "Poppins";
-    font-weight: bold;
-    padding: 20px 20px;
-    outline: none;
-  `);
-
   search.size(600, 50);
-  search.position(50, windowHeight / 2);
+  search.position(0, windowHeight / 2);
   search.center('horizontal');
 
   button = createButton('Let\'s go!');
-  button.style(`
-    border: 0;
-    border-radius: 25px;
-    background: #F7567C;
-    color: #FCFCFC;
-    font-weight: bold;
-    font-family: "Poppins";
-    transition: transform 200ms ease-in-out;
-  `);
   button.size(150, 50);
-  button.position(50, search.y + 100);
+  button.position(0, search.y + 100);
   button.center('horizontal');
 
-  button.mouseOver(_ => {
-    button.style('transform: scale(1.05);');
-  });
+  for (let i = 0; i < 4; i++) {
+    options[i] = i == 3 ? createSlider(2002, 2022, 2022, 1) : createSlider(2, 5, 2, 1);
+    options[i].size(200, 5);
 
-  button.mouseOut(_ => {
-    button.style('transform: scale(1)');
-  });
+    if (windowWidth < 1000) {
+      options[i].position(windowWidth / 2, windowHeight / 2 + 75 * i);
+    } else {
+      options[i].position((i % 2 + 1) * windowWidth / 4 + 80, windowHeight / 2 + 200 * (Math.floor(i / 2)));
+    }
 
-  button.mousePressed(mousePressedButton);
-  button.mouseReleased(_ => {
-    button.style('filter: brightness(100%);');
-  });
+    options[i].hide();
+  }
+
+  button.mouseReleased(mouseReleasedButton);
 
   textAlign(CENTER);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  search.position(0, windowHeight / 2);
   search.center('horizontal');
+
+  button.position(0, windowHeight / 2 + (optioning ? 275 : 100));
   button.center('horizontal');
+
+  for (let i = 0; options.length; i++) {
+    if (windowWidth < 1000) {
+      options[i].position(windowWidth / 2, windowHeight / 2 + 75 * i);
+    } else {
+      options[i].position((i % 2 + 1) * windowWidth / 4 + 80, windowHeight / 2 + 200 * (Math.floor(i / 2)));
+    }
+  }
 }
 
-function mousePressedButton() {
-  button.style('filter: brightness(85%);');
-  console.log('pressed');
+function mouseReleasedButton() {
+  if (!optioning) {
+    search.hide();
+    button.position(0, windowHeight / 2 + 275);
+    button.center('horizontal');
+  
+    for (let i = 0; i < options.length; i++) {
+      options[i].show();
+    }
+  
+    optioning = true;  
+  } else { // User picked options, time to hide everything and show the result
+    button.hide();
+    for (let i = 0; i < options.length; i++) {
+      options[i].hide();
+    }
+
+    optioning = false;
+    resulting = true;
+
+    generation = select('#generation');
+    generation.show();
+
+    spantext = "You should drop out, immediately!";
+  }
+}
+
+function mouseOverSlider() {
+  if (!optioning) {
+    return;
+  }
+
+  for (let i = 0; i < options.length; i++) {
+    const xoff = (i % 2 == 1 && windowWidth >= 1000) ? 250 : -50;
+    const align = (i % 2 == 1) && windowWidth >= 1000 ? LEFT : RIGHT;
+  
+    strokeWeight(0);
+    textSize(28);
+    textFont('Poppins');
+    textStyle(BOLD);
+    textAlign(align);
+    fill('#FCFCFC');
+    text(`${option_labels[i]}: ${options[i].value()}`, options[i].x + xoff, options[i].y + 10);  
+  }
+}
+
+let curchar = 0;
+let curtime = 0;
+
+function animateText() {
+  if (!generation) {
+    return;
+  }
+
+  const display = spantext.slice(0, curchar);
+
+  if (curtime++ > deltaTime / 20) {
+    curtime = 0;
+    curchar++;
+  }
+
+  generation.html(`<span>${display}</span>`);
 }
 
 function handleCircles() {
@@ -109,16 +174,19 @@ function handleCircles() {
 function draw() {
   background('#99E1D9');
 
-  let logox = search.x + 135;
-  let logoy = search.y - 200;
+  let logox = windowWidth / 2 - 175;
+  let logoy = Math.max(windowHeight / 2 - 200, 25);
 
   image(logo, logox, logoy, 100, 100);
 
   textSize(48);
   textFont('Poppins');
   textStyle(BOLD);
+  textAlign(CENTER);
   fill('#FCFCFC');
   text('Jayway', logox + 225, logoy + 65);
 
   handleCircles();
+  mouseOverSlider();
+  animateText();
 }
